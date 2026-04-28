6415bee9eeb2923031350bc7554e696bd38d9258
Chintham Ramakrishna <chintham.ramakrishna@kantar.com>
2026-04-28 16:12:15 +0530

    new changes added-key

--- variables.tf
+++ variables.tf
@@ -50,3 +50,13 @@ variable "inst_count" {
   type        = number
 
 }
+
+variable "access_key" {
+  description = "The access key for the AWS account"
+  type        = string
+}
+
+variable "secret_key" {
+  description = "The secret key for the AWS account"
+  type        = string  
+}
